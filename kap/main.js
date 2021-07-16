4024823bd5ca6b1bf445cf999103c402edb7fc5c
netvoram <30433667+NI0FREE@users.noreply.github.com>
2021-07-16 23:08:04 +0700

    Add files via upload

--- kap/main.js
+++ kap/main.js
@@ -1,3 +1,9 @@
 //const orange = new Controller("box");
 startApp();
 const screenList = [{id:"menu",status: "1"},{id:"notyLayer",status: "1"}]
+
+document.getElementById("sendMoney").addEventListener("click",()=>{
+
+    showNoty("К большому сожалению, перевод Флорбов на данный момент невозможен!",2)
+
+})
