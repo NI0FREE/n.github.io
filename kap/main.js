216e26b924fbf037f89abad69b48da4c2a7194fe
netvoram <30433667+NI0FREE@users.noreply.github.com>
2021-07-17 17:08:30 +0700

    Add files via upload

--- kap/main.js
+++ kap/main.js
@@ -1,13 +1,15 @@
 //const orange = new Controller("box");
 startApp();
-const screenList = [{id:"menu",status: "0",name:"Меню"},{id:"cardPlace",status: "0",name:"Управление средствами"}];
-const header     = document.getElementById("vkHeaderText");
+const backbtn = `<div onclick=" screenShow('menu');" style=" width: 30px; color: #efb96b; margin-top: calc((55px / 2) - 30px / 2); margin-left: 10px; float: left; "><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="browser_back_24"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M8.656 12l7.122 7.122a1.1 1.1 0 11-1.556 1.556l-7.9-7.9a1.1 1.1 0 010-1.556l7.9-7.9a1.1 1.1 0 011.556 1.556L8.656 12z" fill="currentColor" fill-rule="nonzero"></path></g></svg></div>`;
+const screenList = [{id:"menu",status: "0",name:"Меню", back:0 },{id:"cardPlace",status: "0",name:" Управление средствами", back: 1}];
+const header     = document.getElementById("head");
 screenShow("menu");
 document.getElementById("sendMoney").addEventListener("click",()=>{
     //showNoty("К большому сожалению, перевод Флорбов на данный момент невозможен!",2)
     screenShow("cardPlace");
     reFix();
 })
+
 
 document.getElementById("card").addEventListener("click",()=>{
    showNoty("Номер успешно скопирован!",1)
@@ -17,10 +19,22 @@ function screenShow(id){
     for(let i = 0; i < screenList.length; i++){
         let layer = document.getElementById(screenList[i].id);
         if(screenList[i].id==id){
-            layer.style.display = "block";
-            header.innerHTML = screenList[i].name;
+            layer.classList.remove('notyAnim-2-2');
+            layer.classList.add('notyAnim-2');
+            setTimeout(()=>{
+                layer.style.display = "block";
+                let back = "";
+                if(screenList[i].back){
+                    back = backbtn;
+                }
+                header.innerHTML = back+ `<h5 id="vkHeaderText">`+ screenList[i].name + `</h5>`;
+            },100)
         }else{
-            layer.style.display = "none";
+            layer.classList.add('notyAnim-2-2');
+            layer.classList.remove('notyAnim-2');
+            setTimeout(()=>{
+                layer.style.display = "none";
+            },100)
         }
     }
 }
