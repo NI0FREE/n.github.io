a8527ad4585bf5fa8958b43d9ba8e109001cd387
netvoram <30433667+NI0FREE@users.noreply.github.com>
2022-10-22 22:20:54 +0700

    Update storyLine.js

--- kap/storyLine.js
+++ kap/storyLine.js
@@ -22,25 +22,27 @@ var storyList =  [{status:0,summ:2311,senderID:1,time:"1320"},
 
 function storyUpdate(){
     var listpayersIDS=[];
+    var listpayersIDS2=[];
     var cardSEND = "1546 5684 1532 8796";
     var cardUPPR = "5245 4788 4114 3857";
     var resultList = '';
     for(let i = 0; i < storyList.length; i++){
         
         listpayersIDS.push(rint(1000000000,9000000000));
+        listpayersIDS2.push(rint(1000000000,9000000000));
         var storyContaner;
         
         if(storyList[i].status){
             storyContaner = 
              `
-             <div class="infoCard"  onclick="`+'showNoty('+' `Время операции:  '+storyList[i].time[0]+storyList[i].time[1]+':'+storyList[i].time[2]+storyList[i].time[3]+'(МСК) <br> Номер карты отправителя: <br>'+cardSEND+' <br> Номер карты получателя: <br>'+cardUPPR+'<br> Сумма операции: '+storyList[i].summ+'f`,2)'+`" style="background: #7ba97a;height: 52px;position: relative;overflow: hidden;">
+             <div class="infoCard"  id="`+listpayersIDS2[i]+`"  onclick="`+'showNoty('+' `Время операции:  '+storyList[i].time[0]+storyList[i].time[1]+':'+storyList[i].time[2]+storyList[i].time[3]+'(МСК) <br> Номер карты отправителя: <br>'+cardSEND+' <br> Номер карты получателя: <br>'+cardUPPR+'<br> Сумма операции: '+storyList[i].summ+'f`,2)'+`" style="opacity:0; background: #7ba97a;height: 52px;position: relative;overflow: hidden;">
              <div id="`+listpayersIDS[i]+`" class="userinf standartTitle" style="font-size: 14px;position: relative;z-index: 10;margin-top: 14.5px;width: calc(50% - 41px);" id="nameMine">Входящий платеж</div>
              <img width="40" height="40" style="width: calc(100% - 22px); margin-left: 11px;border-radius: 50%;width: auto;height: 70%;margin: 6px;border: 2px solid rgb(255 255 255);color: rgb(191, 192, 193);float: right;margin-left: 10px;" class="Avatar" src="https://sun9-79.userapi.com/impg/lUxuZEZF4Sm8xNpwF2LB0ZFdf-1as8ETbGLa1A/Kywn0yGLdLk.jpg?size=360x360&amp;quality=96&amp;sign=29230a8470d53f622d849a474a165054&amp;type=album"><div class="userinf standartTitle" style="position: relative;z-index: 10;margin-top: 14.5px;width: calc(49% - 41px);text-align: end;margin-left: 0;margin-right: 0;">+ `+storyList[i].summ+`f</div></div>
              `
         }else{
             storyContaner = 
             `
-            <div class="infoCard"  onclick="`+'showNoty('+' `Время операции:  '+storyList[i].time[0]+storyList[i].time[1]+':'+storyList[i].time[2]+storyList[i].time[3]+'(МСК) <br> Номер карты отправителя: <br>'+cardSEND+' <br> Номер карты получателя: <br>'+cardUPPR+'<br> Сумма операции: '+storyList[i].summ+'f`,2)'+`"  style="background: #7a91a9;height: 52px;position: relative;overflow: hidden;">
+            <div class="infoCard"  id="`+listpayersIDS2[i]+`"  onclick="`+'showNoty('+' `Время операции:  '+storyList[i].time[0]+storyList[i].time[1]+':'+storyList[i].time[2]+storyList[i].time[3]+'(МСК) <br> Номер карты отправителя: <br>'+cardSEND+' <br> Номер карты получателя: <br>'+cardUPPR+'<br> Сумма операции: '+storyList[i].summ+'f`,2)'+`"  style="opacity:0; background: #7a91a9;height: 52px;position: relative;overflow: hidden;">
             <div id="`+listpayersIDS[i]+`" class="userinf standartTitle" style="font-size: 14px;position: relative;z-index: 10;width: calc(50% - 41px);margin-top: 14.5px;" id="nameMine">Исходящий платеж</div>
             <img width="40" height="40" style="width: calc(100% - 22px); margin-left: 11px;border-radius: 50%;width: auto;height: 70%;margin: 6px;border: 2px solid rgb(255 255 255);color: rgb(191, 192, 193);float: right;margin-left: 10px;" class="Avatar"  src="https://sun9-79.userapi.com/impg/lUxuZEZF4Sm8xNpwF2LB0ZFdf-1as8ETbGLa1A/Kywn0yGLdLk.jpg?size=360x360&amp;quality=96&amp;sign=29230a8470d53f622d849a474a165054&amp;type=album"><div class="userinf standartTitle" style="position: relative;z-index: 10;margin-top: 14.5px;width: calc(49% - 41px);text-align: end;margin-left: 0;margin-right: 0;" id="nameMine">-  `+storyList[i].summ+`f</div></div>
             `
@@ -48,9 +50,24 @@ function storyUpdate(){
         resultList+=storyContaner;
 
     }
+
+    
+
     document.getElementById("storyLine").innerHTML=resultList;
     for(let i = 0;i< listpayersIDS.length; i ++){
         centerFix2(listpayersIDS[i]);
+        
+    }
+    var time= 100;
+    for(let i = 0;i< listpayersIDS2.length; i ++){
+        let $fix = document.getElementById(listpayersIDS2[i]);
+        
+        setTimeout(()=>{
+           $fix.classList.add('notyAnim-33');
+           $fix.style.opacity = 1;
+        },time);
+        time+=25;
+        //centerFix3(listpayersIDS2[i]);
     }
     
 
@@ -58,6 +75,17 @@ function storyUpdate(){
 
 
 function centerFix2(id){
+    
     let $fix = document.getElementById(id);
     $fix.style.marginTop = (document.getElementById(id).parentElement.clientHeight/2) - (document.getElementById(id).clientHeight/2)-1 +"px";
+
 }
+// function centerFix3(id){
+//     let $fix = document.getElementById(id);
+    
+//     setTimeout(()=>{
+//             $fix.classList.remove('notyAnim-2-2');
+//             $fix.classList.add('notyAnim-2');
+//            // $fix.style.opacity = 1;
+//     },100);
+// }
